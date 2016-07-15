27b98768c4cc2fdf81687b5de8e0c3f4a6a2ed31
Zhukova Veronika <veronikazhukova9@gmail.com>
2016-07-15 10:07:26 +0300

    call button

--- app/components/map.ts
+++ app/components/map.ts
@@ -12,7 +12,6 @@ import {Place} from './../providers/place/place';
 export class Map {
 
     myMap:any;
-    // private PlaceProvider:Place;
 
     constructor(private navController:NavController, private PlaceProvider: Place) {
 
@@ -21,25 +20,10 @@ export class Map {
     public ngAfterViewInit(): void {
 
         var osmUrl = 'http://tiles.maps.sputnik.ru//{z}/{x}/{y}.png',
-            osmAttribution = '',//'Map data <a target="_blank" href="http://www.openstreetmap.org">OpenStreetMap.org</a>; contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            osmAttribution = '',
             osmLayer = new L.TileLayer(osmUrl, {maxZoom: 18, attribution: osmAttribution});
 
-        var map = new L.Map('mapid', {center: new L.LatLng(55.8, 37.7), zoom: 7, layers: [osmLayer]});
-        var baseMaps = {
-
-            "OpenStreetMap": osmLayer
-
-        };
-
-        var greenIcon = L.icon({
-            iconUrl: './build/res/point.png',
-
-            iconSize:     [15, 15], // size of the icon
-            shadowSize:   [15, 15], // size of the shadow
-            iconAnchor:   [15, 15], // point of the icon which will correspond to marker's location
-            shadowAnchor: [15, 15],  // the same for the shadow
-            popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-        });
+        var map = new L.Map('mapid', {center: new L.LatLng(55.8, 37.7), zoom: 7, layers: [osmLayer], zoomControl:false});
 
         map.on('click', onMapClick);
 
@@ -47,20 +31,13 @@ export class Map {
 
          this.PlaceProvider.get().then((data: any) => {
               map.setView(L.latLng(data.latitude, data.longitude), 14);
-              L.marker([data.latitude, data.longitude], {icon: greenIcon}).addTo(map);
              console.log(data.latitude, data.longitude)
 
-             setTimeout(() => map.panTo([data.latitude + 0.001, data.longitude +0.001]), 1000);
 
         }).catch((err) => {
             //debugger
         })
 
-        //map.invalidateSize(true);
-
-        //map._onResize(() => {
-        //    map.invalidateSize(true);
-        //})
 
         var popup = new L.Popup();
 
@@ -70,17 +47,6 @@ export class Map {
 
             popup.setLatLng(e.latlng);
 
-            //popup.setContent("Координаты точки " + latlngStr);
-
-            //map.open/Popup(popup);
-
-            //this.myMap = L.map('mapid').setView([51.505, -0.09], 13);
-            //L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            //    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-            //    maxZoom: 18,
-            //    id: 'your.mapbox.project.id',
-            //    accessToken: 'your.mapbox.public.access.token'
-            //}).addTo(this.myMap);
         }
 
     }
