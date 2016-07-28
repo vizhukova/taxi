da13846e65fc4428a8a75dd5289ab46b9a7e72e8
taran-a <kalipsso96@gmail.com>
2016-07-28 17:47:30 +0300

    Tab favorite in progress

--- app/pages/feed-tab/feed-tab.ts
+++ app/pages/feed-tab/feed-tab.ts
@@ -10,12 +10,26 @@ export class FeedTabPage {
     address: 'Избранные адреса',
     trip: 'Избранные поездки'
   };
+  addresses: any;
+  trips: any;
 
   constructor() {
 
-
+    this.addresses = [
+      {title: "Дом", data: {street: 'Комсомольская 69, п.1'}},
+      {title: "Моя работа", data: {street: 'Петрозаводская 45'}},
+      {title: "Работа жены", data: {street: 'Комсомольская 69, п.1'}},
+      {title: "Детский сад", data: {street: 'Петрозаводская 45'}}
+    ];
+    
+    this.trips = [
+      {title: "Дом", data: {from: 'Комсомольская 69, п.1', to: 'Большая Серпуховская, 64'}},
+      {title: "Моя работа", data: {from: 'Петрозаводская 45', to: 'Большая Серпуховская, 64'}},
+      {title: "Работа жены", data: {from: 'Комсомольская 69, п.1', to: 'Большая Серпуховская, 64'}},
+      {title: "Детский сад", data: {from: 'Петрозаводская 45', to: 'Большая Серпуховская, 64'}}
+    ]
+    
   }
-
 
   toggleView() {
     this.address = !this.address
